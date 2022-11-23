e099431f78049f0defcf5948a8147a7a661e043e
Tales-1 <j.naz248@outlook.com>
2022-11-23 19:17:18 +0000

    Created variants file for framer

---
+++ src/projects/variants.ts
@@ -0,0 +1,59 @@
+ export const imgMotion = {
+    restDesktop:{
+        x:"125px",
+        opacity:.6,
+        transition:{
+            duration:.4,
+            type:"tween",
+            ease:"easeIn"
+        }, 
+    },
+    rest:{
+        x:0,
+        opacity:.6,
+        transition:{
+            duration:.4,
+            type:"tween",
+            ease:"easeIn"
+        }, 
+    },
+    hover: { 
+        x:0,
+        opacity:1, 
+        transition:{
+            duration:0.4,
+            type:"spring",
+            ease:"easeOut"
+        }
+    }
+}
+
+export const cardMotion = { 
+    restDesktop:{
+        x:"-125px",
+        opacity:1,
+        transition:{
+            duration:.4,
+            type:"tween",
+            ease:"easeIn"
+        },
+        
+    },
+    rest:{
+        x:0,
+        opacity:1,
+        transition:{
+            duration:.4,
+            type:"tween",
+            ease:"easeIn"
+        },
+    },
+    hover: { 
+        x:0,
+        transition:{
+            duration:0.4,
+            type:"spring",
+            ease:"easeOut"
+        }
+    }
+}
