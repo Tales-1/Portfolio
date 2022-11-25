6350566d692738a301cfa22609cb34d552464946
Tales-1 <j.naz248@outlook.com>
2022-11-25 16:32:59 +0000

    Created array with social media icons

---
+++ src/assets/iconsArray.ts
@@ -0,0 +1,34 @@
+import GithubIcon from "./github.png"
+import GmailIcon from "./gmail-white.png"
+import LinkedIn from "./linkedin-white.png"
+import TwitterIcon from "./twitter-white.png"
+
+interface IconData {
+    src:string
+    alt:string
+    link:string
+}
+
+export const iconsArray:IconData[] = [
+    {
+        src:GithubIcon,
+        alt:"Github Logo",
+        link:"https://github.com/Tales-1"
+
+    },
+    {
+        src:GmailIcon,
+        alt:"G-mail logo",
+        link:"mailto:jawad.nazir248@gmail.com"
+    },
+    {
+        src:LinkedIn,
+        alt:"LinkedIn logo",
+        link:"https://www.linkedin.com/in/jawad-nazir-31129b240/"
+    },
+    {
+        src:TwitterIcon,
+        alt:"Twitter logo",
+        link:"https://twitter.com/JayNaz97"
+    }
+]
