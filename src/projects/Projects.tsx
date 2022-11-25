5488ad1ffce0f514942fec34cb2df935075d2615
Tales-1 <j.naz248@outlook.com>
2022-11-25 16:31:08 +0000

    Added animation to elements

--- src/projects/Projects.tsx
+++ src/projects/Projects.tsx
@@ -1,15 +1,18 @@
 import { motion, useScroll } from "framer-motion"
 import { useEffect, useState } from "react"
 import ProjectCards from "./ProjectCards"
+
 type Props = {
+    toggle:() => void
     visible:boolean
 }
 
-const Projects:React.FC<Props> = ({visible}) => {
+
+const Projects:React.FC<Props> = ({toggle,visible}) => {
     const [width,setWidth] = useState(window.innerWidth)
     const desktop = width > 1275 
     const { scrollYProgress } = useScroll()
-
+  
     useEffect(()=>{
         window.addEventListener("resize", () => {
             setWidth(window.innerWidth)
@@ -21,21 +24,24 @@ const Projects:React.FC<Props> = ({visible}) => {
             })
         }
     },[])
-    
 
     return (
-        <section className="border-b-2 border-white flex flex-col justify-center gap-16 h-fit py-12 w-full">
+        <motion.section 
+            className="relative bg-light border-b-2 border-white flex flex-col justify-center gap-16 h-fit py-12 w-full"
+            initial={{opacity:0}}
+            animate={{opacity:1, transition:{duration:0.4, delay:1.2}}}
+            >
              <motion.div 
-                className="absolute h-4/5 w-[3px] bg-white left-[50%] top-24"
+                className="absolute h-4/5 w-[3px] bg-white left-[50%] top-32 z-0"
                 initial={{scaleY:0}}
                 style={{scaleY:scrollYProgress, originY:"top"}}>
                 </motion.div>
                 
-                <h2 className="text-3xl lg:text-4xl text-center text-react">&#60;Projects /&gt;</h2>
-                <ul className="grid justify-center w-full gap-12">
+                <h1 className="text-center font-bold text-3xl lg:text-4xl text-[#8892b0]">&#60;Projects /&gt;</h1>
+                <ul className="grid justify-center w-full gap-12 z-10">
                     <ProjectCards />
                 </ul>
-        </section>
+        </motion.section>
     )
 }
 
