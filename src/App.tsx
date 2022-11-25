dfd91dff9c0a1304f2de670b156196e34e873265
Tales-1 <j.naz248@outlook.com>
2022-11-25 16:25:16 +0000

    Refactored & restyled. Fixed transition bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,44 @@
-import { useState } from "react"
-import { motion } from "framer-motion"
-import { Routes, Route } from "react-router-dom"
-import NavigationTree from "./NavigationTree"
+import { useState, useEffect } from "react"
+import { Routes, Route, useLocation } from "react-router-dom"
 import Home from "./Home"
 import About from "./About"
 import Projects from "./projects/Projects"
 import Contact from "./Contact"
 import WelcomeScreen from "./WelcomeScreen"
-
+import { AnimatePresence } from "framer-motion"
+import Header from "./Header"
 
 const App:React.FC = () => {
     const [visible, setVisible] = useState(true)
-    const [showTree, setShowTree] = useState(false)
-    function toggleTree(){
-        setShowTree(prev => !prev)
-    }
-    setTimeout(()=>setVisible(false), 2200)
-
+    const location = useLocation()
+    function toggleTransition(){
+        setVisible(true)
+        setTimeout(()=>setVisible(false),1200)
+    } 
+   
+    setTimeout(() => setVisible(false),1200)
+    
     return (
-        <div className="text-white grid">
-            <WelcomeScreen visible={visible}/>
-            <motion.header 
-                className={`w-screen flex text-react h-[12vh] bg-dark grid grid-flow-col gap-12
-                items-center overflow-hidden`}
-                animate={{height:showTree ? "80vh" : "12vh", transition:{duration:0.5}}}> 
+            <div className="relative text-white flex flex-col min-h-[100vh] bg-light z-0 w-full">
+                <WelcomeScreen visible={visible} />
+                <Header toggleTrans={toggleTransition} />
+                    <main className="bg-light flex flex-col items-center z-10 h-[90vh]">
+                        <AnimatePresence>
+                            <Routes location={location} key={location.pathname}> 
+                                <Route path="/">
+                                    <Route index element={<Home />} />
+                                    <Route path="about" element={<About visible={visible} toggle={toggleTransition}/>}/>
+                                    <Route path="projects" element={<Projects visible={visible} toggle={toggleTransition} />}/>
+                                    <Route path="contact" element={<Contact visible={visible} toggle={toggleTransition} />}/>
+                                </Route>
 
-                <div className="flex flex-col gap-2 mt-4 text-center">
-                    <span className="lg:text-xl">DOM Tree</span>
-                    <button className="text-xs lg:text-base underline hover:opacity-40" onClick={toggleTree}>
-                    {showTree ? "Hide" : "Show"}
-                    </button>
-               </div>
-
-               <NavigationTree toggleTree={toggleTree} />
-            </motion.header>
-
-            <main className="h-full bg-light flex flex-col items-center h-fit z-20">
-                <Routes>
-                    <Route path="/">
-                        <Route index element={<Home />} />
-                        <Route path="about" element={<About />}/>
-                        <Route path="projects" element={<Projects visible={visible} />}/>
-                        <Route path="contact" element={<Contact />}/>
-                    </Route>
-                </Routes>
-            </main>
-        </div>
+                            </Routes>
+                        </AnimatePresence>
+                    </main>
+                   
+            </div>
+        
+       
     )
 }
 
