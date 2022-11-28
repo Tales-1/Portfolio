3857c3a25d2bf7303f98bf620ac4e1a13c86c110
Tales-1 <j.naz248@outlook.com>
2022-11-28 21:46:35 +0000

    Altered styles

--- src/pages/projects/ProjectCards.tsx
+++ src/pages/projects/ProjectCards.tsx
@@ -22,14 +22,13 @@ const ProjectCards:React.FC = () => {
 
     const displayProjects = projectData.map((item,i) => {
         const {name,src,github,live,description,tools} = item
-        const displayTools = tools.map((tool,i) => <li className="text-react" key={i}>{tool}</li>)
+        const displayTools = tools.map((tool,i) => <li className="text-light" key={i}>{tool}</li>)
 
         return (
-            <motion.li className="flex flex-col md:flex-row max-w-[70rem] w-5/6 m-auto"
+            <motion.li className="flex flex-col lg-2:flex-row max-w-[70rem] w-3/5 lg-2:w-5/6 m-auto"
                         initial="rest" whileHover="hover" key={i}
-                        animate= {desktop ? "restDesktop" : "rest"}
-        
-            >
+                        animate= {desktop ? "restDesktop" : "rest"}>
+
                     <motion.div className="w-full aspect-[1.2/1] flex flex-col 
                         rounded-t-lg md:rounded-t-none md:rounded-tl-lg md:rounded-bl-lg overflow-hidden
                         shadow-card"
@@ -38,17 +37,19 @@ const ProjectCards:React.FC = () => {
                         <img src={src} alt="thumbnail of project" className="object-cover h-full w-full" />
                     </motion.div>
 
-                    <motion.div className="grid grid-cols-2 place-items-center bg-dark w-full 
+                    <motion.div className="grid grid-cols-2 place-items-center bg-title w-full 
                         rounded-b-lg md:rounded-b-none md:rounded-br-lg md:rounded-tr-lg overflow-hidden
                         shadow-card"
                         variants={cardMotion}>
-                        <a href={item.live} target="#" className="text-center row-start-1 col-start-1 col-span-full ">
-                            <h1 className="py-3 font-poppins font-bold underline underline-offset-4 text-react hover:opacity-60">
-                                &#60;{name} /&gt;
+                        <a href={item.live} target="#" className="text-center row-start-1 col-start-1 col-span-full">
+                            <h1 className="py-3 font-poppins font-bold underline underline-offset-4 text-light hover:opacity-60">
+                                {name}
                             </h1>
                         </a>
                         
-                        <p className="col-start-1 col-span-full p-6 text-justify row-start-2 md:px-12 font-body text-white">{description}</p>
+                        <p className="col-start-1 col-span-full text-sm lg:text-base p-6 text-justify row-start-2 md:px-12 font-body text-light leading-7">
+                            {description}
+                        </p>
                         
                         <div className="col-start-2 row-start-3 flex items-center gap-6 ml-auto mr-8">
                             <motion.a href={github} target="#"
@@ -61,8 +62,8 @@ const ProjectCards:React.FC = () => {
                             </motion.a>
                         </div>
                         
-                        <ul className="row-start-4 col-start-1 col-span-full flex flex-wrap 
-                        gap-6 justify-center bg-light w-full p-4 mt-6 self-end">
+                        <ul className="row-start-4 col-start-1 col-span-full flex flex-wrap
+                        gap-6 justify-center bg-body w-full p-6 mt-6 self-end text-xs lg-2:text-base">
                             {displayTools}
                         </ul>
 
