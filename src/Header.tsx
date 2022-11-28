c750c171c3443c4969be8e5392e66d8e2ee843e1
Tales-1 <j.naz248@outlook.com>
2022-11-28 21:43:57 +0000

    Added blur to the background

--- src/Header.tsx
+++ src/Header.tsx
@@ -16,17 +16,20 @@ const Header:React.FC = () => {
 
     return ( 
         <>
-        <header className={`w-screen  flex gap-4 relative h-[10vh] `}>  
+        <header className={`mx-auto flex gap-4 relative h-[10vh] w-3/5`}>  
             <nav className="m-auto w-5/6 max-w-[40rem]">
              {mobile ? 
                 <img src={Hamburger} alt="hamburger" onClick={()=>setVisible(true)} 
-                     className="w-6 m-right"
+                     className="w-6 relative right-16"
                 /> : <NavLinks /> }
                 
             </nav>
            
         </header>
-        <SlidingMenu translateX={visible ? "translate-x-0" : "-translate-x-full"} close={()=>setVisible(false)} />
+        <SlidingMenu translateX={visible ? "translate-x-0" : "-translate-x-full"} close={()=>setVisible(false)} mobile={mobile} />
+        <div className={`fixed h-screen w-screen backdrop-blur-sm bg-dark/30 z-20 ${visible ? "visible" : "hidden"}`}
+             onClick={() => setVisible(false)}>
+        </div>
         </>
         
     )
