953d2d7628d0d83ef6cc4ba786a61ca015a99509
Tales-1 <j.naz248@outlook.com>
2022-11-28 21:46:05 +0000

    Passed function and mobile boolean value into Menu

--- src/SlidingMenu.tsx
+++ src/SlidingMenu.tsx
@@ -3,16 +3,17 @@ import NavLinks from "./NavLinks"
 type Props = {
     translateX:string
     close:() => void
+    mobile?:boolean
 }
 
-const SlidingMenu:React.FC<Props> = ({translateX, close}) => {
+const SlidingMenu:React.FC<Props> = ({translateX, close,mobile}) => {
 
     return (
-            <aside className={`fixed left-0 transition-all duration-500 ${translateX} h-screen w-2/3 z-20 bg-dark flex flex-col`}>
+            <aside className={`fixed left-0 transition-all duration-500 ${translateX} h-screen w-2/3 z-30 bg-dark flex flex-col`}>
                 
                 <nav className="flex flex-col h-1/2 m-auto w-full">
                     <span className="m-auto text-white text-3xl cursor-pointer relative bottom-20" onClick={close}>x</span>
-                    <NavLinks flex={"flex-col"} />
+                    <NavLinks flex="flex-col" closeMenu={close} mobile={mobile} />
                 </nav>
             </aside>
 
