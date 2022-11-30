07b6c3c4c41bf9a8c67643f9e5450f4184d05569
Tales-1 <j.naz248@outlook.com>
2022-11-30 14:04:50 +0000

    Removed setTimeout and configured a new route.

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,23 @@ import About from "./pages/About"
 import Projects from "./pages/projects/Projects"
 import Contact from "./pages/Contact"
 import Header from "./Header"
+import ViewProject from "./pages/projects/ViewProject"
 
 const App:React.FC = () => {
-    const [visible, setVisible] = useState(true)
     const location = useLocation()
-   
-    setTimeout(() => setVisible(false),1500)
-    console.log(visible)
     return (
             <div className="relative flex flex-col min-h-[100vh] z-0 w-full bg-[#3b4f4f]">
                 
                 <Header  />
-                    <main className="flex flex-col items-center z-10 w-full lg:w-[70%] mx-auto bg-light lg:rounded-xl shadow-card">
+                    <main className="flex flex-col items-center z-10 w-full mt-2 lg:w-[70%] mx-auto bg-light lg:rounded-xl shadow-card">
                             <Routes location={location} key={location.pathname}> 
                                 <Route path="/">
                                     <Route index element={<Home />} />
                                     <Route path="about" element={<About />}/>
-                                    <Route path="projects" element={<Projects  />}/>
+                                    <Route path="projects">
+                                        <Route index element={<Projects  />}/>
+                                        <Route path=":pageRoute" element={<ViewProject />} />
+                                    </Route>
                                     <Route path="contact" element={<Contact  />}/>
                                 </Route>
 
