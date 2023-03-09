ce73aa448d94be23195de56bf304227e1a8fe127
jawad.nazir124@gmail.com <jawad.nazir124@gmail.com>
2023-03-09 22:04:22 +0000

    used prettier to format tailwind styles

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -21,14 +21,14 @@ const Home: React.FC = () => {
   });
 
   return (
-    <motion.section className="relative w-full h-[75vh] lg:h-[80vh]">
-      <div className="flex flex-col items-center h-full">
-        <h1 className="absolute text-center font-bold text-2xl xl:text-3xl text-react top-8 underline underline-offset-8 ">
+    <motion.section className="relative h-[75vh] w-full lg:h-[80vh]">
+      <div className="flex h-full flex-col items-center">
+        <h1 className="absolute top-8 text-center text-2xl font-bold text-react underline underline-offset-8 xl:text-3xl ">
           Home
         </h1>
-        <motion.div className="my-auto lg:justify-center flex flex-col md:items-center max-w-[45rem] w-4/5 ">
+        <motion.div className="my-auto flex w-4/5 max-w-[45rem] flex-col items-center lg:justify-center ">
           <motion.h2
-            className="text-[2.4rem] xl:text-[4.2rem] font-bold font-poppins text-title"
+            className="font-poppins text-[2.4rem] font-bold text-title xl:text-[4.2rem]"
             initial={{ opacity: 0, y: y }}
             animate={{
               opacity: 1,
@@ -37,7 +37,7 @@ const Home: React.FC = () => {
             }}
           >
             <motion.span
-              className="block text-xs xl:text-[0.81rem] xl:mb-3 text-react"
+              className="block text-xs text-react xl:mb-3 xl:text-[0.81rem]"
               initial={{ opacity: 0, y: y }}
               animate={{
                 opacity: 1,
@@ -50,7 +50,7 @@ const Home: React.FC = () => {
             JAWAD NAZIR
           </motion.h2>
           <motion.span
-            className="text-sm md:text-[1.1rem] xl:text-[1.7rem] mt-4 xl:mt-8 text-react font-poppins underline underline-offset-8 decoration-dashed"
+            className="mt-4 font-poppins text-sm text-react underline decoration-dashed underline-offset-8 md:text-[1.1rem] xl:mt-8 xl:text-[1.7rem]"
             initial={{ opacity: 0, y: y }}
             animate={{
               opacity: 1,
@@ -61,7 +61,7 @@ const Home: React.FC = () => {
             Front-End Developer
           </motion.span>
           <motion.p
-            className="text-xs xl:text-base block text-[#282d35] md:text-center mt-12 md:w-[50ch] lg-2:w-[65ch] 2xl:text-lg"
+            className="mt-12 block text-center text-xs text-[#282d35] md:w-[50ch] xl:text-base 2xl:text-lg lg-2:w-[65ch]"
             initial={{ opacity: 0, y: y }}
             animate={{
               opacity: 1,
@@ -79,9 +79,9 @@ const Home: React.FC = () => {
           <motion.a
             href={require("../assets/jawad-resume.pdf")}
             download="Resume.pdf"
-            className="relative p-[.8rem] xl:p-4 w-fit top-12 text-xs xl:text-base lg:top-[3.5rem] text-white before:border-2 before:border-react 
-                                        cursor-pointer before:absolute before:w-full before:h-full bg-body before:left-[4px] before:bottom-[4px]
-                                        rounded-lg before:rounded-lg shadow-card 2xl:text-lg 2xl:px-4
+            className="relative top-12 w-fit cursor-pointer rounded-lg bg-body p-[.8rem] text-xs text-white shadow-card before:absolute 
+                                        before:left-[4px] before:bottom-[4px] before:h-full before:w-full before:rounded-lg before:border-2 before:border-react
+                                        lg:top-[3.5rem] xl:p-4 xl:text-base 2xl:px-4 2xl:text-lg
                                         "
             initial={{ opacity: 0, y: y }}
             animate={{
@@ -97,8 +97,8 @@ const Home: React.FC = () => {
         </motion.div>
 
         <motion.ul
-          className="flex w-full justify-evenly items-center md:max-w-[20rem] p-2 xl:max-w-[25rem] xl:p-3 gap-2 mx-auto  
-                                bg-body md:rounded-xl md:mb-4 shadow-card relative"
+          className="relative mx-auto flex w-full items-center justify-evenly gap-2 bg-body p-2 shadow-card  
+                                md:mb-4 md:max-w-[20rem] md:rounded-xl xl:max-w-[25rem] xl:p-3"
           initial={{ opacity: 0, y: -y }}
           animate={{ opacity: 1, y: 0, transition: { delay: delay + 0.8 } }}
         >
@@ -106,7 +106,7 @@ const Home: React.FC = () => {
         </motion.ul>
       </div>
 
-      <h4 className="font-poppins fixed right-0 bottom-0 z-10 text-[0.75rem] lg:m-3 text-white">
+      <h4 className="fixed right-0 bottom-0 z-10 font-poppins text-[0.75rem] text-white lg:m-3">
         Designed & built with <span className="text-red-500">&#10084;</span> by
         <span className="text-white"> Jawad Nazir.</span>
       </h4>
