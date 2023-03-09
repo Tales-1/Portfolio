01c07be2580ee25da7bd8408a12840c2fbf6d941
jawad.nazir124@gmail.com <jawad.nazir124@gmail.com>
2023-03-09 22:03:59 +0000

    used prettier to format tailwind styles

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,37 +1,52 @@
-import { motion } from "framer-motion"
-import { useState } from "react"
-import ContactForm from "../components/ContactForm"
+import { motion } from "framer-motion";
+import { useState } from "react";
+import ContactForm from "../components/ContactForm";
 
-const Contact:React.FC= () => {
-    const [submitted, setSubmitted] = useState(false)
-    const handleSubmit = () => {
-        setTimeout(() => {
-          setSubmitted(true);
-        }, 100);
-      };
-    return ( 
-        <section className="relative w-full flex flex-col gap-12 h-fit xl:h-[83vh] pb-8">
-                <h1 className="text-center font-bold text-2xl xl:text-3xl text-react w-full mt-8 underline underline-offset-8">Contact</h1>
-                <motion.div className={`text-center flex flex-col gap-6 lg:gap-12 items-center w-4/5 text-white ${submitted ? "" : ""} m-auto`}
-                                initial={{opacity:0, y:-50}}
-                                animate={{opacity:1, y:0, transition:{duration:0.7}}}>
-                        {submitted ? 
-                                <div className="grid gap-12 relative bottom-8">
-                                    <h2 className="text-[2.8rem] lg:text-7xl font-poppins font-bold text-title">Thank <span className="text-react">you!</span></h2>
-                                    <p className="text-sm font-body tracking-wider text-body lg:text-xl">I'll be in touch soon.</p>
-                                </div>
-                            : 
-                                <div className="flex flex-col gap-8 2xl:gap-10 relative 2xl:bottom-4">
-                                    <h2 className="text-[2.4em] md:text-[2.5rem] 2xl:text-[4rem] font-poppins font-bold text-title">Get in <span className="text-react">Touch.</span></h2>
-                                    <p className="text-sm font-body tracking-wider text-body 2xl:text-lg">Want a clean and friendly interface for your app?
-                                    <span className="block mt-2">Well, what are you waiting for!</span> </p>
-                                    <ContactForm handleSubmit = {handleSubmit}/>
-                                </div>
-                        }
-                </motion.div>
-        </section>
-    )
-}
+const Contact: React.FC = () => {
+  const [submitted, setSubmitted] = useState(false);
+  const handleSubmit = () => {
+    setTimeout(() => {
+      setSubmitted(true);
+    }, 100);
+  };
+  return (
+    <section className="relative flex h-fit w-full flex-col gap-12 pb-8 xl:h-[83vh]">
+      <h1 className="mt-8 w-full text-center text-2xl font-bold text-react underline underline-offset-8 xl:text-3xl">
+        Contact
+      </h1>
+      <motion.div
+        className={`flex w-4/5 flex-col items-center gap-6 text-center text-white lg:gap-12 ${
+          submitted ? "" : ""
+        } m-auto`}
+        initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: 1, y: 0, transition: { duration: 0.7 } }}
+      >
+        {submitted ? (
+          <div className="relative bottom-8 grid gap-12">
+            <h2 className="font-poppins text-[2.8rem] font-bold text-title lg:text-7xl">
+              Thank <span className="text-react">you!</span>
+            </h2>
+            <p className="font-body text-sm tracking-wider text-body lg:text-xl">
+              I'll be in touch soon.
+            </p>
+          </div>
+        ) : (
+          <div className="relative flex flex-col gap-8 2xl:bottom-4 2xl:gap-10">
+            <h2 className="font-poppins text-[2.4em] font-bold text-title md:text-[2.5rem] 2xl:text-[4rem]">
+              Get in <span className="text-react">Touch.</span>
+            </h2>
+            <p className="font-body text-sm tracking-wider text-body 2xl:text-lg">
+              Want a clean and friendly interface for your app?
+              <span className="mt-2 block">
+                Well, what are you waiting for!
+              </span>{" "}
+            </p>
+            <ContactForm handleSubmit={handleSubmit} />
+          </div>
+        )}
+      </motion.div>
+    </section>
+  );
+};
 
-
-export default Contact
+export default Contact;
